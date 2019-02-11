f0a719bf9a42ef02cae8a44e30cc30dfea303131
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2019-02-11 14:24:07 -0700

    Agregar archivos de proyecto.

---
+++ Herencia2/Estudiante.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia2
+{
+    class Estudiante: Persona
+    {
+        public int Matricula { get; set; }
+        public string Grado { get; set; }
+
+        public Estudiante(string nombre, int edad, int matricula, string grado): base(nombre, edad)
+        {
+            Matricula = matricula;
+            Grado = grado;
+        }
+        public void Estudiar()
+        {
+            Console.WriteLine("{0} esta estudiando como puerco \n", Nombre);
+        }
+        public override void Saludar()
+        {
+            Console.WriteLine("Hola, mi nombre es {0}, tengo {1} años, mi matricula es {2} y estoy en {3} grado"
+                , Nombre, Edad, Matricula, Grado);
+        }
+    }
+}
---
+++ Herencia2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p1 = new Persona("Carlos Vegeta", 45);
+            p1.Saludar(); 
+
+            Empleado e1 = new Empleado("Alfonso D´Askr", 18, "Lider", 5000);
+            e1.Saludar();
+            e1.Trabajar();
+
+            Estudiante es1 = new Estudiante("Poyo Jon", 21, 420911, "Cuarto");
+            es1.Saludar();
+            es1.Estudiar();
+
+            Console.ReadKey();
+        }
+    }
+}
